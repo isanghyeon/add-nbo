08dc4420cc1ac30d76c8dcbce0b6e3da9472d1ed
isanghyeon <cpd4268@naver.com>
2021-07-21 03:18:28 +0900

    add-nbo module

---
+++ src/add-nbo.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 이상현 on 2021/07/21.
+//
+
+#include "add-nbo.h"
+
---
+++ src/add-nbo.h
@@ -0,0 +1,10 @@
+//
+// Created by 이상현 on 2021/07/21.
+//
+
+#ifndef SRC_ADD_NBO_H
+#define SRC_ADD_NBO_H
+#include <stdio.h>
+
+uint32_t add_nbo(uint32_t 1st_input, uint32_t 2rd_input)
+#endif //SRC_ADD_NBO_H
