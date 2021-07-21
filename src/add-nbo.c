cdb7f513602d0e18d8248d40bdb2b9226c72e723
isanghyeon <cpd4268@naver.com>
2021-07-21 11:38:57 +0900

    add-nbo module

--- src/add-nbo.c
+++ src/add-nbo.c
@@ -21,14 +21,14 @@ uint32_t add_nbo(uint32_t first_input, uint32_t second_input) {
             (second_input & 0x000000FF) << 24
     };
 
-    uint32_t returnInput = 0;
+    uint32_t returnInputSum = 0;
     uint32_t tmp1 = 0, tmp2 = 0;
 
-    for (uint32_t i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++) {
         tmp1 |= first_input_binary[i];
         tmp2 |= second_input_binary[i];
     }
-    returnInput = tmp1 + tmp2;
+    returnInputSum = tmp1 + tmp2;
     // 1000(0x3e8) + 500(0x1f4) = 1500(0x5dc) => 출력
-    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", tmp1, tmp1, tmp2, tmp2, returnInput, returnInput);
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", tmp1, tmp1, tmp2, tmp2, returnInputSum, returnInputSum);
 }
